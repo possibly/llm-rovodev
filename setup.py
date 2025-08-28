3b977a7b943e38322c0c2b8002f170cee08c7e85
Tyler Brothers <tylerbrothers1@gmail.com>
2025-08-28 03:14:57 +0000

    bump v0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="llm-rovodev",
-    version="0.1.2",
+    version="0.1.3",
     description="LLM plugin exposing the RovoDev model for the llm CLI",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
